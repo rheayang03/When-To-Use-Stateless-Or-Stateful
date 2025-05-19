986b692f2eea03e60305853c17d53f7556d00a46
rhea_yang03 <rheamaenavares7@gmail.com>
2025-05-19 16:10:54 +0800

    Update main.dart with new context

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/chat_page.dart';
 
 void main() {
-  runApp(ChatApp());
+  runApp(const ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text('Hello World!');
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
   }
 }
