8d9c90b5da79f94738c31d69deb965070c342d55
rhea_yang03 <rheamaenavares7@gmail.com>
2025-05-21 11:15:57 +0800

    Update chat_page.dart with flexible and expanded layout

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -20,55 +20,21 @@ class ChatPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          // Message list
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              children: [
+              children: const [
                 MessageCard(isSent: false),
                 MessageCard(isSent: true),
                 MessageCard(isSent: false),
+                MessageCard(isSent: true),
               ],
             ),
           ),
+          // Chat input bar
           const InputBar(),
         ],
-      ),
-    );
-  }
-}
-
-class MessageCard extends StatelessWidget {
-  final bool isSent;
-  const MessageCard({super.key, required this.isSent});
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: isSent ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        constraints: const BoxConstraints(maxWidth: 250),
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: isSent ? Colors.grey : Colors.grey,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment:
-              isSent ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Hi, this is your message',
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            const SizedBox(height: 8),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              height: 150,
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -87,12 +53,10 @@ class InputBar extends StatelessWidget {
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
       child: Row(
-        children: const [
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.add, color: Colors.white),
-          ),
-          Expanded(
+        children: [
+          const Icon(Icons.add, color: Colors.white),
+          const SizedBox(width: 12),
+          const Expanded(
             child: TextField(
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(
@@ -102,11 +66,46 @@ class InputBar extends StatelessWidget {
               ),
             ),
           ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.send, color: Colors.white),
-          ),
+          const SizedBox(width: 12),
+          const Icon(Icons.send, color: Colors.white),
         ],
+      ),
+    );
+  }
+}
+
+class MessageCard extends StatelessWidget {
+  final bool isSent;
+  const MessageCard({super.key, required this.isSent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isSent ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 250),
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: isSent ? Colors.grey[800] : Colors.grey[700],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment:
+              isSent ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Hi, this is your message',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+            const SizedBox(height: 8),
+            Image.network(
+              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+              height: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
