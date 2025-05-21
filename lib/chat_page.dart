5fd34e71d77305ebede2cd53aaf5d91ef0bb7310
rhea_yang03 <rheamaenavares7@gmail.com>
2025-05-21 11:21:52 +0800

    Restore chat_page.dart to previous state

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -20,54 +20,17 @@ class ChatPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          // Message list
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              children: const [
+              children: [
                 MessageCard(isSent: false),
                 MessageCard(isSent: true),
                 MessageCard(isSent: false),
-                MessageCard(isSent: true),
               ],
             ),
           ),
-          // Chat input bar
           const InputBar(),
-        ],
-      ),
-    );
-  }
-}
-
-class InputBar extends StatelessWidget {
-  const InputBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.add, color: Colors.white),
-          const SizedBox(width: 12),
-          const Expanded(
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: 'Type a message...',
-                hintStyle: TextStyle(color: Colors.white54),
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-          const SizedBox(width: 12),
-          const Icon(Icons.send, color: Colors.white),
         ],
       ),
     );
@@ -85,9 +48,9 @@ class MessageCard extends StatelessWidget {
       child: Container(
         constraints: const BoxConstraints(maxWidth: 250),
         margin: const EdgeInsets.symmetric(vertical: 10),
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(24),
         decoration: BoxDecoration(
-          color: isSent ? Colors.grey[800] : Colors.grey[700],
+          color: isSent ? Colors.grey : Colors.grey,
           borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
@@ -106,6 +69,44 @@ class MessageCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class InputBar extends StatelessWidget {
+  const InputBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      child: Row(
+        children: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.add, color: Colors.white),
+          ),
+          Expanded(
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Type a message...',
+                hintStyle: TextStyle(color: Colors.white54),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.send, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
